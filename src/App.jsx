173cef62b8a64d334cc2831089bd219050ab59f6
Md-Abdullah1 <abdullahahil153@gmail.com>
2024-02-05 18:06:03 +0530

    updated components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TodoInput from "./components/TodoInput"
 
 const App = () => {
   return (
-    <div className="container w-full min-h-full flex justify-center items-start p-4">
+    <div className="container w-full min-h-full max-w-[100vw]  flex justify-center items-start p-4">
       <TodoInput/>
       {/* <Todos/> */}
     </div>
